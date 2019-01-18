e7c8ea5c5d764f39485aa50d6c221d27e7eab655
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 20:53:36 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -11,9 +11,12 @@ class CreateTodo extends Component {
   }
 
   handleSubmit = event => {
-    event.preventDefault();
-    this.props.addTodo(this.state)
-  }
+  event.preventDefault();
+  this.props.addTodo(this.state)
+  this.setState({
+    text: '',
+  })
+}
 
   handleChange(event) {
     this.setState({
