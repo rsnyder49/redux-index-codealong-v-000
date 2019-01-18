11ff25ebb202140443e2b0e7d530953a3c8acbc5
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 20:52:07 +0000

    Automatically backed up by Learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -9,5 +9,12 @@ class TodosContainer extends Component {
     );
   }
 };
+
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
  
-export default connect()(TodosContainer);
+export default connect(mapStateToProps)(TodosContainer);
+ 
